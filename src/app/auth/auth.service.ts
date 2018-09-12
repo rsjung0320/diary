e289b7150c4e83c88f5f98329d24697302171ad9
raesung <rsjung0320@gmail.com>
2018-09-12 14:24:52 +0900

    수정

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -63,7 +63,7 @@ export class AuthService {
           this.router.navigate(['/']);
         }
       }, err => {
-        this.authStatusListener.next(true);
+        this.authStatusListener.next(false);
       });
   }
 
